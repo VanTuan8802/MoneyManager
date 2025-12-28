67ec137e0a6efe2e262bef6e4178852eb688fd6f
VanTuan8802 <vantuan8802@gmail.com>
2025-12-28 21:06:03 +0700

    int project

---
+++ MoneyManager/MoneyManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyManagerApp.swift
+//  MoneyManager
+//
+//  Created by VanTuan8802 on 28/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MoneyManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
