9a4dbdbff26261c9df2bd0a187eecfa680b35d87
VanTuan8802 <vantuan8802@gmail.com>
2025-12-29 20:54:25 +0700

    add font and icon app

---
+++ MoneyManager/Commons/Extention/Font+.swift
@@ -0,0 +1,52 @@
+//
+//  Font+.swift
+//  MoneyManager
+//
+//  Created by VanTuan8802 on 29/12/25.
+//
+
+import Foundation
+import SwiftUI
+
+enum FontFamily {
+    case regular
+    case medium
+    case semiBold
+    case bold
+
+    var font: String {
+        switch self {
+        case .regular:
+            return "Inter-Regular"
+        case .medium:
+            return "Inter-Medium"
+        case .semiBold:
+            return "Inter-Semibold"
+        case .bold:
+            return "Inter-Bold"
+        }
+    }
+}
+
+extension Font {
+    /// Regular
+    static let regular8 = Font.custom(FontFamily.regular.font, size: 8)
+    static let regular16 = Font.custom(FontFamily.regular.font, size: 16)
+
+    /// Medium
+    static let medium10 = Font.custom(FontFamily.medium.font, size: 10)
+    static let medium12 = Font.custom(FontFamily.medium.font, size: 12)
+    static let medium16 = Font.custom(FontFamily.medium.font, size: 16)
+    static let medium20 = Font.custom(FontFamily.medium.font, size: 20)
+
+    /// SemiBold
+    static let semibold12 = Font.custom(FontFamily.semiBold.font, size: 12)
+    static let semibold14 = Font.custom(FontFamily.semiBold.font, size: 14)
+    static let semibold16 = Font.custom(FontFamily.semiBold.font, size: 16)
+    static let semibold20 = Font.custom(FontFamily.semiBold.font, size: 20)
+    static let semibold24 = Font.custom(FontFamily.semiBold.font, size: 24)
+
+    /// Bold
+    static let bold12 = Font.custom(FontFamily.bold.font, size: 12)
+
+}
